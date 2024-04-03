22f90bb031c6484fa31bf2e862afa3b0162835fd
GreenMoose <juggernaut78@gmail.com>
2024-04-03 12:20:08 +0200

    Add test support (https://nextjs.org/docs/app/building-your-application/testing/jest) [try/wallabyWithNextJsAndTs]

---
+++ jest.setup.ts
@@ -0,0 +1,2 @@
+﻿//Good to know: extend-expect was removed in v6.0, so if you are using @testing-library/jest-dom before version 6, you will need to import @testing-library/jest-dom/extend-expect instead.
+import '@testing-library/jest-dom'
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <main>
+            <h1>Foo</h1>
+        </main>
+    );
+}
